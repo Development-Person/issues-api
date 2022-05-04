51bd441e0ebb7108e591c61d79009f6329795a4d
Ibrahim Achkar <ibaemail@gmail.com>
2022-05-04 11:37:43 +1000

    express serving array of issues from data.js

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-const express = require('express')
-const app = express()
-const port = 3000
+import express from 'express';
+const app = express();
+const port = 3000;
+import { data } from './data.js';
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  res.send(data);
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
