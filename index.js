e83a7a68d12e611f4378bd4e72f83d856c3a7657
Ibrahim Achkar <ibaemail@gmail.com>
2022-05-04 12:42:12 +1000

    added update

--- index.js
+++ index.js
@@ -41,21 +41,56 @@ app.post('/issues/create', (req, res) => {
 // READ
 // Return all issues (returned as an array)
 app.get('/issues', (req, res) => {
-  res.send(data);
+  //1. bringing in the current issues data from our "db"
+  const issuesData = JSON.parse(fs.readFileSync('./data.json'));
+  //2. send issuesData
+  res.send(issuesData);
 });
 
 // Return single issue based on req param of id
 app.get('/issues/:id', (req, res) => {
-  // 1. get requested id from params
+  //1. bringing in the current issues data from our "db"
+  const issuesData = JSON.parse(fs.readFileSync('./data.json'));
+  // 2. get requested id from params
   //issue ids are being stored as ints, hence parsing int
   const requestedId = parseInt(req.params.id);
-  // 2. find the issue that matches the id
-  const issue = data.find((issue) => issue.id === requestedId);
-  // 3. return the issue
+  // 3. find the issue that matches the id
+  const issue = issuesData.find((issue) => issue.id === requestedId);
+  // 4. return the issue
   res.send(issue);
 });
 
 // UPDATE
+app.patch('/issues/:id/update', (req, res) => {
+  //1. bringing in the current issues data from our "db"
+  const issuesData = JSON.parse(fs.readFileSync('./data.json'));
+
+  // 2. get requested id from params
+  //issue ids are being stored as ints, hence parsing int
+  const requestedId = parseInt(req.params.id);
+
+  //3. getting the title and description from the body of the request
+  const issueTitle = req.body.title;
+  const issueDescription = req.body.description;
+
+  // 4. find the index of the object in the array that matches the id
+  const issueIndex = issuesData.findIndex((issue) => issue.id === requestedId);
+
+  // 5. updating the data if it exists in the body
+  if (issueTitle) {
+    issuesData[issueIndex].title = issueTitle;
+  }
+
+  if (issueDescription) {
+    issuesData[issueIndex].description = issueDescription;
+  }
+
+  //6. Writing the entire issuesData array from memory to the file (overwriting existing)
+  fs.writeFileSync('./data.json', JSON.stringify(issuesData, null, 2));
+
+  // 5. return the issue
+  res.send(issuesData[issueIndex]);
+});
 
 // DELETE
 
